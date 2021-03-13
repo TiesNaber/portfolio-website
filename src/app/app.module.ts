f54d93eade1284de2414ece697fca2640e260caf
Ties Naber <tiesnaber@live.nl>
2021-03-13 19:45:29 +0100

    Generated portfolio-main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PortfolioMainComponent } from './portfolio-main/portfolio-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioMainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/portfolio-main/portfolio-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PortfolioMainComponent } from './portfolio-main.component';
+
+describe('PortfolioMainComponent', () => {
+  let component: PortfolioMainComponent;
+  let fixture: ComponentFixture<PortfolioMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PortfolioMainComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PortfolioMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
